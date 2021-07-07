c6948a0778af589224f0916c31625be4e140801f
AndreNew <andrecmrb@gmail.com>
2021-07-07 14:49:06 -0400

    criar lista cliente historia

---
+++ GerenciamentoClientes/GerenciamentoClientes.Historias/ClienteHistoria/ListarClientes.cs
@@ -0,0 +1,33 @@
+﻿using GerenciamentoClientes.Historias.ClienteHistoria.ViewModels;
+using GerenciamentoClientes.Infra.Contexto;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GerenciamentoClientes.Historias.ClienteHistoria
+{
+    public class ListarClientes
+    {
+        private readonly GerenciamentoClienteContexto _contexto;
+
+        public ListarClientes(GerenciamentoClienteContexto contexto)
+        {
+            _contexto = contexto;
+        }
+        public async Task<List<ClienteViewModel>> Executar()
+        {
+            var clientes = await _contexto.Cliente.ToListAsync();
+
+            var clientesViewModel = clientes.Select(cliente => {
+
+                ClienteViewModel clienteViewModel = cliente;
+
+                return clienteViewModel;
+
+            }).ToList();
+
+            return clientesViewModel;
+        }
+    }
+}
--- GerenciamentoClientes/GerenciamentoClientes.Historias/ClienteHistoria/ViewModels/ClienteViewModel.cs
+++ GerenciamentoClientes/GerenciamentoClientes.Historias/ClienteHistoria/ViewModels/ClienteViewModel.cs
@@ -8,19 +8,40 @@ namespace GerenciamentoClientes.Historias.ClienteHistoria.ViewModels
 
         public Guid Id { get; set; } = Guid.NewGuid();
 
-        public string Nome { get;  set; }
+        public string Nome { get; set; }
 
-        public string CPF { get;  set; }
+        public string CPF { get; set; }
 
-        public DateTime DataNascimento { get;  set; }
+        public DateTime DataNascimento { get; set; }
 
         public DateTime DataCadastro { get; set; } = DateTime.Now;
 
-        public decimal RendaFamiliar { get;  set; }
+        public decimal RendaFamiliar { get; set; }
 
         public static implicit operator Cliente(ClienteViewModel clienteViewModel)
         {
-            return new Cliente(id: clienteViewModel.Id, nome: clienteViewModel.Nome, cpf: clienteViewModel.CPF, dataNascimento: clienteViewModel.DataNascimento, dataCadastro: clienteViewModel.DataCadastro, rendaFamiliar: clienteViewModel.RendaFamiliar);
+            return new Cliente(
+                id: clienteViewModel.Id,
+                nome: clienteViewModel.Nome,
+                cpf: clienteViewModel.CPF,
+                dataNascimento: clienteViewModel.DataNascimento,
+                dataCadastro: clienteViewModel.DataCadastro,
+                rendaFamiliar: clienteViewModel.RendaFamiliar);
+        }
+
+        public static implicit operator ClienteViewModel(Cliente cliente)
+        {
+            return new ClienteViewModel {
+                Id = cliente.Id,
+                Nome = cliente.Nome,
+                CPF = cliente.CPF,
+                DataNascimento = cliente.DataNascimento,
+                DataCadastro = cliente.DataCadastro,
+                RendaFamiliar = cliente.RendaFamiliar
+            };
         }
     }
 }
+
+
+
