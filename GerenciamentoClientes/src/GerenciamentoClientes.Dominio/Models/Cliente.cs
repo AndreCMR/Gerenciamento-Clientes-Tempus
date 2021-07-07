2369ee423e078d83c9ee8f6497d0085693fb1485
AndreNew <andrecmrb@gmail.com>
2021-07-07 13:38:07 -0400

    criar camada de dominio

---
+++ GerenciamentoClientes/src/GerenciamentoClientes.Dominio/Models/Cliente.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GerenciamentoClientes.Dominio.Models
+{
+    public class Cliente
+    {
+        protected Cliente()
+        {
+             //
+        }
+
+        public Cliente(Guid id, string nome, string cPF, DateTime dataNascimento, DateTime dataCadastro, decimal rendaFamiliar)
+        {
+            Id = id;
+            Nome = nome;
+            CPF = cPF;
+            DataNascimento = dataNascimento;
+            DataCadastro = dataCadastro;
+            RendaFamiliar = rendaFamiliar;
+        }
+
+        public Guid Id { get; private set; }
+
+        public string Nome { get; private set; }
+
+        public string CPF { get; private set; }
+
+        public DateTime DataNascimento { get; private set; }
+
+        public DateTime DataCadastro { get; private set; }
+
+        public decimal RendaFamiliar { get; private set; }
+    }
+}
